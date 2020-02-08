903d883a7537e0b3240fa49da425c315f1d79ff9
EliezerTeixeira <eliezerteixeira@outlook.pt>
2020-02-08 13:15:20 +0100

    added validation to StudentController

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Student;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 
 class StudentController extends Controller
 {
@@ -16,6 +17,31 @@ class StudentController extends Controller
     }
 
     public function addNewStudent(Request $request){
+
+        $messages = [
+            // mensagem de erro para um campo especifico
+            // ex: 'phone.required' , 'phone.min', 'first_name.unique' etc...
+            // mensagem de erro para todos os campos.
+            'required' => 'O :attribute deve estar preenchido', // :attribute vai referenciar o nome do campo a ser preenchido
+            'min' => 'O :attribute deve conter no minino :min caracteres', // :min indica o valor minimo a ser aceite
+            'max' => 'O :attribute de conter no máximo :max caracteres', // :max indica o valor máximo a ser aceite
+            'unique' => 'O :attribute já existe nos nossos registros',
+            'email' => 'Por favor preencha um :attribute válido'
+        ];
+
+        $validation = Validator::make($request->all(), [
+            'name' => 'required|string|min:2|max:60', // regras de validação
+            'email' => 'required|email|unique:students|max:100',
+            'phone' => 'required|string|unique:students|min:9|max:12',
+            'birthdate' => 'required|date'
+        ], $messages);
+
+        if ($validation->fails()){
+
+            return response()->json( // Retornar a mensagem de erro
+                $validation->errors()
+            );
+        }
 
         $students = new Student();
         $students->name = $request['name'];
