0ef6ce993fa81a965aeeddd9e5388086ed117dc7
EliezerTeixeira <eliezerteixeira@outlook.pt>
2020-02-08 13:31:20 +0100

    making a migrate:fresh

--- database/migrations/2020_02_08_110028_create_course_student_table.php
+++ database/migrations/2020_02_08_110028_create_course_student_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCourseStudentsTable extends Migration
+class CreateCourseStudentTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateCourseStudentsTable extends Migration
      */
     public function up()
     {
-        Schema::create('course_students', function (Blueprint $table) {
+        Schema::create('course_student', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->unsignedBigInteger('course_id');
             $table->unsignedBigInteger('student_id');
@@ -36,6 +36,6 @@ class CreateCourseStudentsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('course_students');
+        Schema::dropIfExists('course_student');
     }
 }
